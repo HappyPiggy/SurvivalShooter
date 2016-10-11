77e094ae8f208b0c3bf99a043621c7ac204cab8a
Pigggger <296056428@qq.com>
2016-10-11 13:40:29 +0800

    first commit

---
+++ Assets/Scripts/Camera/CamFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollow : MonoBehaviour
+{
+
+    public float speed;
+    public Transform PlayerPos;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - PlayerPos.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetPos = PlayerPos.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPos, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public int camRayLength = 100;
+
+    private Rigidbody rb;
+    private Vector3 movement;
+    private int floorMask;
+    private Animator animator;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        rb=GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0.0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+       // Debug.Log(movement);
+        rb.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+       
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength))
+        {
+           // Debug.Log("test");
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0.0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            rb.MoveRotation(newRotation);
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0 || v != 0;
+        //Debug.Log(walking);
+        animator.SetBool("IsWalking", walking);
+    }
+}
