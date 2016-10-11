bdf73730b5a06537e8e8391db689800152d2c9c8
Pigggger <296056428@qq.com>
2016-10-11 20:58:49 +0800

    add damage UI

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -53,7 +53,7 @@ public class EnemyAttack : MonoBehaviour
 
         if(playerHealth.currentHealth <= 0)
         {
-            anim.SetTrigger ("PlayerDead");
+            anim.SetTrigger ("PlayerDie");
         }
     }
 
